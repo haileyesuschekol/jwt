5c7655ac9ec8bad09ee1c0f78c8952ed8b7b9ce9
Haileyesus Chekol <teamdevs7@gmail.com>
2024-11-23 10:28:21 +0000

    refactor

--- errors/index.js
+++
@@ -1,9 +0,0 @@
-const CustomAPIError = require("./custom-error")
-const BadRequest = require("./bad-request")
-const unauthenticated = require("./unauthenticated")
-
-module.exports = {
-  CustomAPIError,
-  BadRequest,
-  unauthenticated,
-}
