e4a41b470f06708232eba04d2de5261b0b92345f
Haileyesus Chekol <teamdevs7@gmail.com>
2024-11-23 09:49:59 +0000

    create custome error class

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require("./custom-error")
+const BadRequest = require("./bad-request")
+const unauthenticated = require("./unauthenticated")
+
+module.exports = {
+  CustomAPIError,
+  BadRequest,
+  unauthenticated,
+}
